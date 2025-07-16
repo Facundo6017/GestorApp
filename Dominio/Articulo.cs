43c11b07f02d91dbf0de8390383e86d28f13e141
Facundo <kufa6017@gmail.com>
2025-07-16 10:28:38 -0300

    Modelado de capas

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public long codigo { get; set; }
+        public string nombre { get; set; }
+        public string marca { get; set; }
+        public string categoria { get; set; }
+        public string urlImg { get; set; }
+        public float precio { get; set; }
+    }
+}
---
+++ Negocio/ArticuloService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class ArticuloService
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> listaArticulo = new List<Articulo>();
+
+            return listaArticulo;
+        }
+    }
+}
